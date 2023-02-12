2693f2bf15882824ebee26a7fe52965c9f41c96b
Connor P. Jackson <cpjackson@berkeley.edu>
2023-02-12 13:48:29 -0800

    Bring in new MDCEV model application structure
    - Apollo initialization and structure now in the separate initialization script
    - Bring in the separate .rda file of the MDCEV model object plus initialization script name
    - change the name of the persons table to avoid namespace conflict
    - save other packages in the renv lockfile
    
    # Conflicts:
    #	main.R

--- Model_application_functions_2017.R
+++ Model_application_functions_2017.R
@@ -2,6 +2,10 @@
 ###Codes for running application related functions###
 #####################################################
 
+library(stringr)
+library(dplyr)
+library(data.table)
+
 ## Process coefs
 # 2
 coef2_prepare <- function(coef2){
@@ -268,173 +272,19 @@ model_application <- function(persons, data1, coefs_name_mile, coefs_mile , coef
   rm(tt1);
 
   # 3. MDECV
-  VehicleFleet_model <- function(data1){ # LJ change: change data to data1
+  source("3_MDCEV_application.r")
 
-    database1 <- data_temp %>% mutate(outside=0, car1=0, car2=0, car3=0, van1=0, van2=0, van3=0, suv1=0, suv2=0, suv3=0,
-                                      pickup1=0, pickup2=0, pickup3=0, motorbike=0) # LJ change to a local variable
+  data_temp <- data_temp %>% dplyr::mutate(annual_nonmotor_new=0, car1=0, car2=0, car3=0, van1=0,
+                                           van2=0, van3=0, suv1=0, suv2=0, suv3=0,
+                                           pickup1=0, pickup2=0, pickup3=0, motorbike=0)
 
-    ###### Apollo function
-    ### Initialise code
-    apollo_initialise()
+  mdcev_predicted <- VehicleFleet_model(data_temp, coef3_model, mdcev_model_spec_file)
 
-    ### Set core controls # LJ change, move to the main function
-
-    # ################################################################# #
-    #### DEFINE MODEL PARAMETERS                                     ####
-    # ################################################################# #
-
-
-
-    # ################################################################# #
-    #### GROUP AND VALIDATE INPUTS                                   ####
-    # ################################################################# #
-
-    apollo_inputs <- apollo_validateInputs(database = database1) # LJ change to <- from <<-; and specify database = database1
-
-    # ################################################################# #
-    #### DEFINE MODEL AND LIKELIHOOD FUNCTION                        ####
-    # ################################################################# #
-
-    apollo_probabilities<-function(apollo_beta = apollo_beta, apollo_inputs = apollo_inputs, functionality="estimate"){ # LJ change to explicit argument assignment
-
-      ### Attach inputs and detach after function exit
-      apollo_attach(apollo_beta, apollo_inputs)
-      on.exit(apollo_detach(apollo_beta, apollo_inputs))
-
-      ### Create list of probabilities P
-      P = list()
-
-      ### Define individual alternatives
-      alternatives  = c("outside", "car1", "car2", "car3",
-                        "van1", "van2", "van3",
-                        "suv1", "suv2", "suv3",
-                        "pickup1", "pickup2", "pickup3",
-                        "motorbike")
-
-      ### Define availabilities
-      avail = list(outside  = 1, car1=1, car2=1, car3=1,
-                   van1=1, van2=1, van3=1,
-                   suv1=1, suv2=1, suv3=1,
-                   pickup1=1, pickup2=1, pickup3=1,
-                   motorbike=1)
-
-      ### Define continuous consumption for individual alternatives
-      continuousChoice = list(outside=outside, car1=car1, car2=car2, car3=car3,
-                              van1=van1, van2=van2, van3=van3,
-                              suv1=suv1, suv2=suv2, suv3=suv3,
-                              pickup1=pickup1, pickup2=pickup2, pickup3=pickup3,
-                              motorbike=motorbike)
-
-      ### Define utilities for individual alternatives
-      V = list()
-      V[["outside"]]  = 0
-      V[["car1"]]     = delta_car1 # + delta_car1_income1*income1 + delta_car1_income4*income4 + delta_car1_NUMCHILD*NUMCHILD
-      V[["car2"]]   = delta_car2 # + delta_car2_income2*income2 + delta_car2_work2*work2
-      V[["car3"]] = delta_car3 # + delta_car3_income1*income1 + delta_car3_retired*retired
-      V[["van1"]]     = delta_van1 # + delta_van1_NUMCHILD*NUMCHILD
-      V[["van2"]]   = delta_van2 # + delta_van2_NUMCHILD*NUMCHILD
-      V[["van3"]] = delta_van3 # + delta_van3_income1*income1 + delta_van3_HHSIZE1*HHSIZE1
-      V[["suv1"]]     = delta_suv1 # + delta_suv1_income1*income1 + delta_suv1_work2*work2
-      V[["suv2"]]   = delta_suv2 # + delta_suv2_income3*income3 + delta_suv2_HHSIZE4*HHSIZE4
-      V[["suv3"]] = delta_suv3 # + delta_suv3_income4*income4 + delta_suv3_child *child
-      V[["pickup1"]]     = delta_pickup1 # + delta_pickup1_HHSIZE1*HHSIZE1 + delta_pickup1_income5*income5
-      V[["pickup2"]]   = delta_pickup2 # + delta_pickup2_child*child + delta_pickup2_income1*income1
-      V[["pickup3"]] = delta_pickup3 # + delta_pickup3_income2*income2 + delta_pickup3_child*child
-      V[["motorbike"]] = delta_motorbike # + delta_motorbike_HHSIZE1*HHSIZE1
-
-      ### Define alpha parameters
-      alpha = list(
-        outside=alpha_base,
-        car1=alpha_base,
-        car2=alpha_base,
-        car3=alpha_base,
-        van1=alpha_base,
-        van2=alpha_base,
-        van3=alpha_base,
-        suv1=alpha_base,
-        suv2=alpha_base,
-        suv3=alpha_base,
-        pickup1=alpha_base,
-        pickup2=alpha_base,
-        pickup3=alpha_base,
-        motorbike=alpha_base)
-
-      ### Define gamma parameters
-      gamma = list( car1=gamma_car1,
-                    car2=gamma_car2,
-                    car3=gamma_car3,
-                    van1=gamma_van1,
-                    van2=gamma_van2,
-                    van3=gamma_van3,
-                    suv1=gamma_suv1,
-                    suv2=gamma_suv2,
-                    suv3=gamma_suv3,
-                    pickup1=gamma_pickup1,
-                    pickup2=gamma_pickup2,
-                    pickup3=gamma_pickup3,
-                    motorbike=gamma_motorbike)
-
-      ### Define costs for individual alternatives
-      cost = list(outside  = 1,
-                  car1=1,
-                  car2=1,
-                  car3=1,
-                  van1=1,
-                  van2=1,
-                  van3=1,
-                  suv1=1,
-                  suv2=1,
-                  suv3=1,
-                  pickup1=1,
-                  pickup2=1,
-                  pickup3=1,
-                  motorbike=1)
-
-      ### Define budget
-      budget = budget
-
-      ### Define settings for MDCEV model
-      mdcev_settings <- list(alternatives      = alternatives,
-                             avail             = avail,
-                             continuousChoice = continuousChoice,
-                             V                 = V,
-                             alpha             = alpha,
-                             gamma             = gamma,
-                             sigma             = sigma,
-                             cost              = cost,
-                             budget            = budget)
-
-      ### Compute probabilities using MDCEV model
-      P[["model"]] = apollo_mdcev(mdcev_settings, functionality)
-
-      ### Take product across observation for same individual
-      # P = apollo_panelProd(P, apollo_inputs, functionality)
-
-      ### Prepare and return outputs of function
-      P = apollo_prepareProb(P, apollo_inputs, functionality)
-      return(P)
-    }
-
-    mdcev = apollo_prediction(model = coef3_model,
-                              apollo_probabilities = apollo_probabilities,
-                              apollo_inputs = apollo_inputs)
-    mdcev <- mdcev[,c(1,3:16)]
-    names(mdcev) <- c("ID", "NONMOTOR", "car1","car2","car3","van1","van2","van3","suv1","suv2","suv3","pickup1","pickup2","pickup3","MOTOR")
-    mdcev$AVG <- 1
-    mdcev <- mdcev %>% dplyr::relocate(AVG, .after=all_of("ID"))
-
-    NumHousehold <- nrow(data1);
-    pnumbyalter <- mdcev;
-    pnumbyalter[pnumbyalter > 0] <- 1;
-    pnumbyalter <- pnumbyalter[,3:(ncol(pnumbyalter))];
-
-    pavgbyalter <- mdcev;
-    pavgbyalter <- colSums(pavgbyalter[,3:(ncol(pavgbyalter))])
-    pavgmileage <- pavgbyalter / NumHousehold;
-
-    return(mdcev);
-  }
-  mdcev <- VehicleFleet_model(data1);
+  mdcev <- dcast(mdcev_predicted, ID ~ outcome, value.var = "cont_mean")
+  mdcev[, AVG := 1]
+  setcolorder(mdcev, c("ID", "AVG", "outside", "car1","car2","car3","van1","van2","van3",
+                       "suv1","suv2","suv3","pickup1","pickup2","pickup3","motorbike"))
+  setDF(mdcev)
 
   # 4. Reallocation
   Heuristic_mileage <- function(data, mdcev) {
--- main.R
+++ main.R
@@ -8,7 +8,7 @@
 
 # the following code has been tested on atlas-test instance in the R container environment
 
-# Tin, please note I used the following before testing the code 
+# Tin, please note I used the following before testing the code
 #docker pull     ghcr.io/lbnl-science-it/atlas:main
 
 # docker run -v /home/ubuntu/AWS/PILATES/pilates/atlas:/mnt -it --entrypoint=bash  ghcr.io/lbnl-science-it/atlas:main
@@ -16,16 +16,16 @@
 
 # command line parse to setup the i/o dir and year to run --------------#
 
-# Note that preceding this docker call, the preprocessing py code already took .h5 output from urbansim 
+# Note that preceding this docker call, the preprocessing py code already took .h5 output from urbansim
 # and extracted various data tables into .csv file
 
-#useparser = F  # currently we do not use the parser for directory and year input
-useparser = T  # we do want to use cmd argument parsing now 
+useparser = F  # currently we do not use the parser for directory and year input
+# useparser = T  # we do want to use cmd argument parsing now -Tin
 
-if(useparser){  
+if(useparser){
   suppressPackageStartupMessages(library("optparse"))
-  
-  option_list <- list( 
+
+  option_list <- list(
     make_option(c("--indir"),  dest="inputdirPath",  action="store", help="path to input  data", default="/atlas_input" ),
     make_option(c("--outdir"), dest="outputdirPath", action="store", help="path to output data", default="/atlas_output" ),
     make_option(c("--basedir"), dest="basedirPath", action="store", help="dir where pilates/orchestrator is located", default="~/Dropbox/Research/SmartGrid_Behavioral/TransportationInitiative/ATLAS/Software_Development/AWS/PILATES" ),
@@ -34,35 +34,35 @@ if(useparser){
     make_option(c("--freq"), dest="freq", action="store", help="simulation interval", default="1" ), # only yearly runs are enabled currently
     make_option(c("--nsample"), dest="nsample", action="store", help="subsample of hh to process, 0 if all hh", default= "0" ), # for atlas_v2, all hhs needs to be processed
     make_option(c("--npe"), dest="npe", action="store", help="number of cores for parallel computing", default="9" ), # number of cores to use in parallel run
-    
+
     # LJ add 5/31/2022, option to read zscore of job accessibility by transit from beam, 1- true, 0- false, will read from the observed data
-    make_option(c("--beamac"), dest="beamac", action="store", 
+    make_option(c("--beamac"), dest="beamac", action="store",
                 help="indicator of whether to read from beam derived zscore of job accessibility by transit", default="0" ) ,
- 
+
     # LJ add 9/21/2022, option to run static or dynamic
-    make_option(c("--mod"), dest="mod", action="store", help="static (1) or dynamic (2) ", default="1" ) 
-    
- 
+    make_option(c("--mod"), dest="mod", action="store", help="static (1) or dynamic (2) ", default="1" )
+
+
   )
-  # output year is the current year in urbansim output, for atlas_v2, prediction of output year will use vehicle predictions from previous years 
+  # output year is the current year in urbansim output, for atlas_v2, prediction of output year will use vehicle predictions from previous years
   # note, static model directly predicts output year without relying on previous years
-  
+
   opt <- parse_args(OptionParser(option_list=option_list))
-  
-  
-  print( "input  directory specified as:") 
+
+
+  print( "input  directory specified as:")
   print( opt$inputdirPath )
   print( "--" )
   print( "output directory specified as:")
   print( opt$outputdirPath )
   print( "--" )
-  print( "codedir  directory specified as:") 
+  print( "codedir  directory specified as:")
   print( opt$codedirPath )
   print( "--" )
-  print( "basedir  directory specified as:") 
+  print( "basedir  directory specified as:")
   print( opt$basedirPath )
   print( "--" )
-  print( "outputyear, freq specified as:") 
+  print( "outputyear, freq specified as:")
   print( opt$outputyear )
   print( opt$freq ) # atlas can run frequency of 1 or 2. and this option is not used in the code yet as usim is running yearly.
   print( "number of clusters for parallel computing")
@@ -72,11 +72,9 @@ if(useparser){
 
   print( "sample of households to process. \nCaution: only works for static version, dynamic version will always use full population")
   if(opt$nsample == 0){ print('full sample')}else{print(opt$nsample)}
-  
-  
-  
+
   # read out the global variables so that subsequent programs can all use them
-  
+
   basedir = opt$basedirPath
   codedir = opt$codedirPath
   inputdir = opt$inputdirPath  # the mounting point
@@ -84,73 +82,73 @@ if(useparser){
   outputyear    = strtoi(opt$outputyear, base=10)
   nsample = strtoi(opt$nsample, base=10) # number of households to subsample
   Npe = strtoi(opt$npe, base=10) # number of processors to use
-  
+
   # read option of whether read from beam derived accessibility, 0, 1
   beamac = strtoi(opt$beamac, base=10)
-  
-  atlas_runmod = strtoi(opt$mod, base=10) # 'static 1' or 'dynamic 2'
-  
-  warmTF = strtoi(opt$warmstart, base=10) # 1=warmstart run, 0=evolution run
-  
-  
-  
 
-  print( "basedir and codedir parsed as:") 
+  atlas_runmod = strtoi(opt$mod, base=10) # 'static 1' or 'dynamic 2'
+
+  warmTF = strtoi(opt$warmstart, base=10) # 1=warmstart run, 0=evolution run
+
+
+
+
+  print( "basedir and codedir parsed as:")
   print( basedir )
   print( codedir )
-  
+
   setwd(codedir)
-  
-  
+
+
   if(atlas_runmod == 2 & nsample !=0){
     stop('Error - atlas run mode 2: dynamic evolution should only run on full sample')
   }
-  
+
 # region specific constants here
   iniyear = 2017 # default initialization year for SF
   # Austin need to change this to 2018
-  
+
 }
 
 ######################################################################################################
 #-------- Below is Ling's code with inputdir, outputdir, inputyear, outputyear manually defined -----#
-# can be modified 
+# can be modified
 if(!useparser){ # if not using parser, define things here for debuging process
-#  codedir = '/Volumes/GoogleDrive/Shared drives/Shared_ATLAS/fromLing/SoftwareDevelopment/sfb_atlas_v2/code_inside_container'  # Note that R is launched from the "code_inside_container" folder
-  codedir = '/mnt/data2/ljin/sfb_atlas_v2/code_inside_container' # if on gems instance
+  # codedir = '/Volumes/GoogleDrive/Shared drives/Shared_ATLAS/fromLing/SoftwareDevelopment/sfb_atlas_v2/code_inside_container'  # Note that R is launched from the "code_inside_container" folder
+  # codedir = '/mnt/data2/ljin/sfb_atlas_v2/code_inside_container' # if on gems instance
   # codedir = '/mnt/code_inside_container'  # Note that R is launched from the "code_inside_container" folder
-# codedir = '/'
+  # codedir = '/'
   # Global dir and variables
   # these are best set as command line arguments to main.R via the optparse above
-#  basedir = '/Volumes/GoogleDrive/Shared drives/Shared_ATLAS/fromLing/SoftwareDevelopment/sfb_atlas_v2'
-  basedir = '/mnt/data2/ljin/sfb_atlas_v2'
+  # basedir = '/Volumes/GoogleDrive/Shared drives/Shared_ATLAS/fromLing/SoftwareDevelopment/sfb_atlas_v2'
+  # basedir = '/mnt/data2/ljin/sfb_atlas_v2'
   #  basedir = '/mnt/'
+  basedir <- getwd()
+  codedir <- "."
   inputdir <- file.path(basedir,'atlas_input')
   outputdir <- file.path(basedir, 'atlas_output')
-  
-  
-  outputyear <- 2019
+
+
+  outputyear <- 2017
   simyear = outputyear
-  
+
   nsample = 0 # 0- full sample, >0 subsample
-  Npe = 40 # number of processors
-  
+  Npe = 4 # number of processors
+
   beamac = 0 # read from observed job accessibility
-  
+
 
   atlas_runmod = 2 # '1-static' or '2-dynamic'
-  
+
   setwd(codedir)
-  
-  iniyear = 2017 # default initialization year 
+
+  iniyear = 2017 # default initialization year
                  # Austin need to change this to 2018
-  
+
   if(atlas_runmod == 2 & nsample !=0){
     stop('Error - atlas run mode 2: dynamic evolution should only run on full sample')
   }
 
-  
-  
 }
 ###########################################
 #     DO NOT MODIFY BELOW !
@@ -159,8 +157,8 @@ if(!useparser){ # if not using parser, define things here for debuging process
 debugTF = F
 
 if(atlas_runmod == 1) {
-  
-  # 1. set up 
+
+  # 1. set up
   library(stats) # LJ add, predict function is used for predicting main driver and ownlease
   library(tidyverse)
   library(dplyr)
@@ -169,18 +167,18 @@ if(atlas_runmod == 1) {
   library(parallel)
   library(doParallel)
   library(foreach)
-  
+
   source('source_atlas_v1.R')
-  
+
  } # static version of atlas, always run atlas_v1
 
 if(atlas_runmod == 2){
-  
-  
+
+
   # # ---- global variables, will be moved to 'global_vars.R' after testing ------------- #
   # # note that I changed some notation
   # iniyear = 2017 # default initialization year
-  
+
   if(outputyear <= iniyear){ # for <=2017, use static initialization
     library(stats) # LJ add, predict function is used for predicting main driver and ownlease
     library(tidyverse)
@@ -190,19 +188,19 @@ if(atlas_runmod == 2){
     library(parallel)
     library(doParallel)
     library(foreach)
-    
+
     source('source_atlas_v1.R')
-    
-    if(outputyear == iniyear){ # for the initial year (2017) of dynamic evolution, 
+
+    if(outputyear == iniyear){ # for the initial year (2017) of dynamic evolution,
                               # map variable name back to names used subsequently by dynamic prediction
                               # save to input folder (as these will be used as inputs for vehicle evolution)
-      
+
       source('clean_dynamic_data_fromv1.R')
-      
+
     } # additional cleaning for initial year outputs and inputs (for SFB it is 2017)
   }else{ # for years >iniyear
     simyear = outputyear
-    
+
     if(((simyear - iniyear)%/%2)*2 - (simyear - iniyear) == 0){
       # evolution year
       evoTF = T
@@ -211,71 +209,71 @@ if(atlas_runmod == 2){
       evoTF = F
       baseyear = simyear - 1 # current wave should be 1 year ago
     }# mid year
-    
+
     if(!evoTF){
       # if it is the simyear is the mid year
       print(paste('year', simyear, 'is a mid year, not a evolution year.'))
       print('proceed with non-evolution prediction.')
-      
+
       # 1. clean data
-      print('cleaning the mid year demo data')      
+      print('cleaning the mid year demo data')
       source('clean_for_mid_prediction.R')
-      
-      
+
+
       # 2. setup: loading global variables and setup function
-      
+
       print('setup mid year prediction')
       source('setup_midyr_prediction.R')
-      
+
       # 3. run prediction for mid year
       print('run mid year prediction')
       source('run_midyr_prediction.R')
-      
+
       # 4. reset colnames and save csv
       print('output csvs')
       source('reset_name_save_csv.R')
-      
-    }else{ # else if evolution year, i.e. need to predict from baseyear = simyear -2 
-      
+
+    }else{ # else if evolution year, i.e. need to predict from baseyear = simyear -2
+
       print(paste('year', simyear, 'is an evolution year, evolve the fleet in this time step'))
       evoyear = simyear
-      
+
       # 1. clean data
       print('clean data for vehicle evolution')
       source('clean_for_evo_prediction.R')
-      
+
       # for debugging purpose, we skip above cleaning step and load the data directly
       if(debugTF){
-        load(file = file.path(inputdir, paste0('year',evoyear),'households0.RData')) #households0, 
+        load(file = file.path(inputdir, paste0('year',evoyear),'households0.RData')) #households0,
         load(file = file.path(inputdir, paste0('year',evoyear),'households1.RData')) # households1
-        load(file = file.path(inputdir, paste0('year',evoyear),'persons1.RData')) # persons1, 
-        load(file = file.path(inputdir, paste0('year',evoyear),'persons0.RData')) #persons0, 
+        load(file = file.path(inputdir, paste0('year',evoyear),'persons1.RData')) # persons1,
+        load(file = file.path(inputdir, paste0('year',evoyear),'persons0.RData')) #persons0,
       }
-      
-      
-      
-      
+
+
+
+
       # 2. setup: loading global variables and setup function
       print('setup evolution year parameters and functions')
       source('setup_evoyr_prediction.R')
-      
+
       # 3. run prediction for evolution year
       print('run evolution year prediction')
       tic()
       source('run_evoyr_prediction.R')
       toc()
-      
+
       #remember to map the adopt vehicle predictions back to atlas vehicle bodytypes so that next round we can use it for vehicle transaction prediction
-      
+
       # 4. reset colnames and save csv
       print('output csvs')
       source('reset_name_save_csv.R')
-      
-    }# else if evolution year, i.e. need to predict from baseyear = simyear -2 
-    
+
+    }# else if evolution year, i.e. need to predict from baseyear = simyear -2
+
   }# dynamic prediction for year > iniyear
-  
-  
+
+
 }# run atlas_v2: for SFB, year <=2017 run static version, after that, dynamic evolve
 
 if(!(atlas_runmod %in% c(1,2))){
